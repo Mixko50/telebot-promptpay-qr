9a12c4391c0651ea9ac9843a9dcfbda563fe1535
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2022-11-21 22:01:47 +0700

    Update init.go

--- utils/config/init.go
+++ utils/config/init.go
@@ -1,20 +1,22 @@
 package config
 
 import (
-	"gopkg.in/yaml.v3"
-	"io/ioutil"
+	"os"
 )
 
 var C = new(config)
 
 func init() {
 	// * Load YAML configuration
-	yml, err := ioutil.ReadFile("config.yml")
-	if err != nil {
-		panic("UNABLE TO READ YAML CONFIGURATION FILE")
-	}
-	err = yaml.Unmarshal(yml, C)
-	if err != nil {
-		panic("UNABLE TO PARSE YAML CONFIGURATION FILE")
-	}
+	// yml, err := ioutil.ReadFile("config.yml")
+	// if err != nil {
+	// 	panic("UNABLE TO READ YAML CONFIGURATION FILE")
+	// }
+	// err = yaml.Unmarshal(yml, C)
+	// if err != nil {
+	// 	panic("UNABLE TO PARSE YAML CONFIGURATION FILE")
+	// }
+	C.PromptPayKbankId = os.Getenv("prompt_pay_kbank_id")
+	C.PromptPayScbId = os.Getenv("prompt_pay_scb_id")
+	C.TelegramToken = os.Getenv("telegram_token")
 }
